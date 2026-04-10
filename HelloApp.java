fe241eab66999a2ceadd711193556337c7b1771f
as5976-EdgE <as5976@srmist.edu.in>
2026-04-10 23:42:25 +0530

    Coded UC3: Display Hello with CLI Argument or Default Message

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,8 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Check if length is greater than 0. If yes, use args[0]. If no, use "World".
+        String name = (args.length > 0) ? args[0] : "World";
+        
+        System.out.println("Hello, " + name + "!");
     }
 }

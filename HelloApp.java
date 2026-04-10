5ec26162738ba90450da4a9b02de3e09b14d2453
as5976-EdgE <as5976@srmist.edu.in>
2026-04-10 23:29:48 +0530

    Implement UC2: Display personalized greeting using CLI arguments

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,11 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        if (args.length > 0) {
+            // Take the first argument and print the greeting
+            System.out.println("Hello, " + args[0] + "!");
+        } else {
+            // Fallback if no name is provided
+            System.out.println("Hello, Guest!");
+        }
     }
 }

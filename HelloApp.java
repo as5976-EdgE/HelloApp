32c633f5fb355212b722a218ab0ed7ceca3e6aa5
as5976-EdgE <as5976@srmist.edu.in>
2026-04-11 00:17:01 +0530

    Implemented UC4: Multiple command-line arguments

--- HelloApp.java
+++ HelloApp.java
@@ -1,9 +1,15 @@
 public class HelloApp {
     public static void main(String[] args) {
-        // UC3: Use Ternary operator to check if an argument exists.
-        // If args.length > 0, use args[0]; otherwise, default to "World".
-        String name = (args.length > 0) ? args[0] : "World";
-        
-        System.out.println("Hello, " + name + "!");
+        String names;
+
+        if (args.length > 0) {
+            // Join all arguments into a single string separated by ", "
+            names = String.join(", ", args);
+        } else {
+            // Fallback to default
+            names = "World";
+        }
+
+        System.out.println("Hello, " + names + "!");
     }
 }
